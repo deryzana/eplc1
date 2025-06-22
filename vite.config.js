ace81134870ae89d12a5925114715a47aa060446
deryzana <deryzana@gmail.com>
2025-06-23 02:25:47 +0700

    Update vite.config.js
    tambahkan: base: '/eplc1/',

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/eplc1/',
   plugins: [react()],
   // server: {
   //   host: 'www.eplc.com', // atau bisa langsung 'www.eplc.com' jika hanya untuk itu
